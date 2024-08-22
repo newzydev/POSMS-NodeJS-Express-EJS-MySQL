dc2884a3a01788c8afc441d6c491c1b419e474bf
NewzyDev <sakdar.user.hcc@gmail.com>
2024-08-22 20:26:03 +0700

    main

--- Routes/Role/Shop_Owner/System_Settings.js
+++ Routes/Role/Shop_Owner/System_Settings.js
@@ -3,11 +3,20 @@ exports.getSystemSettingPage = (req, res) => {
     const your_page = 'System_Settings';
     const error = req.flash('error');
     const success = req.flash('success');
-    
-    res.render('Role/Shop_Owner/System_Settings', { 
-        title, 
-        your_page,
-        error: error[0],
-        success: success[0]
+
+    // Query to retrieve system settings from the database
+    const query = 'SELECT * FROM Systen_Settings';
+
+    db.query(query, (err, result) => {
+        if (err) throw err;
+
+        const settings = result[0];
+        res.render('Role/Shop_Owner/System_Settings', { 
+            title, 
+            your_page,
+            error: error[0],
+            success: success[0],
+            settings
+        });
     });
 };
