4483cc07ae87fbee109db87917ffbdc188b0df48
Sakdar Sukkwan <sakdar.user.hcc@gmail.com>
2024-09-23 12:10:03 +0700

    main

--- Middlewares/setting.js
+++ Middlewares/setting.js
@@ -1,15 +1,15 @@
 // System Setting Middlewares
 const System_Settings = (req, res, next) => {
+    const settings = res.locals.settings;
     const query = 'SELECT * FROM Systen_Settings';
 
     db.query(query, (err, results) => {
         if (err) {
-            return res.status(500).send('Internal Server Error');
+            return res.status(500).render('Error_Page/500', { title: 'INTERNAL SERVER ERROR - ' + settings.text_footer });
         }
 
         // ตั้งค่าเป็น res.locals เพื่อให้เรียกใช้ได้กับทุกหน้าและทุกใฟล์
         res.locals.settings = results.length ? results[0] : {};
-
         next();
     });
 };
